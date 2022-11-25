50e8227a88e9fb647316e33e394efb6990722474
mariorizki <mariorizki07@gmail.com>
2022-11-25 15:18:02 +0700

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,14 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, StyleSheet, Text, View } from "react-native";
+import SafeViewAndroid from "./util/SafeViewAndroid";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={SafeViewAndroid.droidSafeArea}>
+      <StatusBar backgroundColor="#eee" barStyle="dark-content" />
+      <View className="">
+        <Text className="">Open up App.js to start working on your app!</Text>
+      </View>
+    </SafeAreaView>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
